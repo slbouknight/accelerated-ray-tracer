3d01f9b5662691597323907aa231e849241ca9fe
Sedrick Bouknight <sedrick@bouknight.io>
2025-08-10 11:33:28 -0400

    Use accelerated vec3 class

--- src/main.cu
+++ src/main.cu
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <time.h>
+#include "vec3.h"
 
 #define checkCudaErrors(val) check_cuda((val), #val, __FILE__, __LINE__)
 void check_cuda(cudaError_t result, char const *const func, const char *const file, int const line)
@@ -7,7 +8,7 @@ void check_cuda(cudaError_t result, char const *const func, const char *const fi
     if (result)
     {
         std::cerr << "CUDA error = " << static_cast<unsigned int>(result) << "at " <<
-        file << ":" << " '" << func << "' \n";
+        file << ":" << line << " '" << func << "' \n";
 
         // Make sure we call CUDA Device Reset before exiting
         cudaDeviceReset();
@@ -15,16 +16,13 @@ void check_cuda(cudaError_t result, char const *const func, const char *const fi
     }
 }
 
-__global__ void render(float *fb, int max_x, int max_y)
+__global__ void render(vec3 *fb, int max_x, int max_y)
 {
     int i = threadIdx.x + blockIdx.x * blockDim.x;
     int j = threadIdx.y + blockIdx.y * blockDim.y;
     if((i >= max_x) || (j >= max_y)) return;
-    int pixel_index = j*max_x*3 + i*3;
-    fb[pixel_index + 0] = float(i) / max_x;
-    fb[pixel_index + 1] = float(j) / max_y;
-    fb[pixel_index + 2] = 0.2;
-
+    int pixel_index = j*max_x + i;
+    fb[pixel_index] = vec3( float(i) / max_x, float(j) / max_y, 0.2f); 
 }
 
 int main()
@@ -34,19 +32,27 @@ int main()
     int tx = 8;
     int ty = 8;
 
+    std::cerr << "Rendering a " << nx << "x" << ny << " image ";
+    std::cerr << "in " << tx << "x" << ty << " blocks.\n";
+
     int num_pixels = nx * ny;
-    size_t fb_size = 3 * num_pixels * sizeof(float);
+    size_t fb_size = num_pixels * sizeof(vec3);
 
     // Allocate frame buffer
-    float *fb;
+    vec3 *fb;
     checkCudaErrors(cudaMallocManaged((void **)&fb, fb_size));
 
+    clock_t start, stop;
+    start = clock();
     // Render buffer
     dim3 blocks(nx/tx+1, ny/ty+1);
     dim3 threads(tx, ty);
     render<<<blocks, threads>>>(fb, nx, ny);
     checkCudaErrors(cudaGetLastError());
     checkCudaErrors(cudaDeviceSynchronize());
+    stop = clock();
+    double timer_seconds = ((double(stop - start)) / CLOCKS_PER_SEC);
+    std::cerr << "took " << timer_seconds << " seconds.\n";
 
     // Output frame buffer as image
     std::cout << "P3\n" << nx << " " << ny << "\n255\n";
@@ -55,13 +61,10 @@ int main()
     {
         for (int i = 0; i < nx; i++)
         {
-            size_t pixel_index = j *3*nx + i*3;
-            float r = fb[pixel_index + 0];
-            float g = fb[pixel_index + 1];
-            float b = fb[pixel_index + 2];
-            int ir = int(255.999*r);
-            int ig = int(255.999*g);
-            int ib = int(255.999*b);
+            size_t pixel_index = j*nx + i;
+            int ir = int(255.99*fb[pixel_index].r());
+            int ig = int(255.99*fb[pixel_index].g());
+            int ib = int(255.99*fb[pixel_index].b());
             std::cout << ir << " " << ig << " " << ib << "\n";
         }
     }
