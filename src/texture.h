bd4f045d6baa7af8689e1a1fa60cd2bdbca72b27
Sedrick Bouknight <sedrick@bouknight.io>
2025-08-23 17:59:37 -0400

    Adding perlin textures

--- src/texture.h
+++ src/texture.h
@@ -1,6 +1,8 @@
 #ifndef TEXTURE_H
 #define TEXTURE_H
+
 #include "color.h"
+#include "perlin.h"
 #include "rtw_stb_image.h"
 
 class texture
@@ -75,6 +77,21 @@ class image_texture : public texture
 
     private:
         rtw_image image;
+};
+
+class noise_texture : public texture
+{
+    public:
+        noise_texture(double scale) : scale(scale) {}
+
+        color value(double u, double v, const point3& p) const override 
+        {
+            return color(.5, .5, .5) * (1 + std::sin(scale * p.z() + 10 * noise.turb(p, 7)));
+        }
+
+        private:
+            perlin noise;
+            double scale;
 };
 
 #endif
