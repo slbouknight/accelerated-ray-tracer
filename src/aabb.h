16f7f51076e43021c527a851cecce38fe4e48cbe
Sedrick Bouknight <sedrick@bouknight.io>
2025-08-24 13:03:03 -0400

    Adding quad primitive

--- src/aabb.h
+++ src/aabb.h
@@ -11,7 +11,10 @@ class aabb
         aabb() {} // empty bounding box by default
 
         aabb(const interval& x, const interval& y, const interval& z)
-            : x(x), y(y), z(z) {}
+            : x(x), y(y), z(z) 
+        {
+            pad_to_minimums();
+        }
 
         // Treat a and b as extrema for bbox, don't need to specify min/max
         aabb(const point3& a, const point3& b)
@@ -19,6 +22,8 @@ class aabb
             x = (a[0] <= b[0]) ? interval(a[0], b[0]) : interval(b[0], a[0]);
             y = (a[1] <= b[1]) ? interval(a[1], b[1]) : interval(b[1], a[1]);
             z = (a[2] <= b[2]) ? interval(a[2], b[2]) : interval(b[2], a[2]);
+
+            pad_to_minimums();
         }
 
         // Construct a bbox given two input boxes
@@ -75,6 +80,17 @@ class aabb
                 return y.size() > z.size() ? 1 : 2;
         }
         static const aabb empty, universe;
+
+    private:
+        void pad_to_minimums() 
+        {
+        // Adjust the AABB so that no side is narrower than some delta, padding if necessary.
+
+        double delta = 0.0001;
+        if (x.size() < delta) x = x.expand(delta);
+        if (y.size() < delta) y = y.expand(delta);
+        if (z.size() < delta) z = z.expand(delta);
+    }
 };
 
 const aabb aabb::empty    = aabb(interval::empty,    interval::empty,    interval::empty);
